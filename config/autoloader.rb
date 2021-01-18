9ca3885df38b885092ae7622fd939388ae19695f
TurVitAn <turzhansky81@gmail.com>
2021-01-18 14:53:02 +0200

    Add basic configuration

---
+++ config/autoloader.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'i18n'
+require 'pry'
+
+require_relative '../lib/console'
---
+++ spec/codebreaker_console/console_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.describe CodebreakerConsole::Console do
+  context 'when run console' do
+    it 'call main menu' do
+      # not implemented yet
+    end
+  end
+end
